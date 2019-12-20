50f2cafc2156c80fd833c19f41e30110e3957a7d
bobi <983430207@qq.com>
2019-12-20 12:50:09 +0800

    04-04-03 管理员列表页

--- app/Http/Controllers/Admin/AdminUserController.php
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -3,13 +3,20 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\AdminUser;
 use Illuminate\Http\Request;
 
 class AdminUserController extends Controller
 {
     //列表
-    public function index(){
-        return view('admin.adminuser.index');
+    public function index(AdminUser $adminuser){
+
+        $adminusers = $adminuser->orderBy('id', 'desc')->get();
+        $data = [
+            'adminusers' => $adminusers,
+        ];
+
+        return view('admin.adminuser.index', $data);
     }
 
     //添加+编辑
