a26e5e6cc948d5b37f4edd1dea9cad62c0b6d6d8
bobi <983430207@qq.com>
2020-01-06 12:32:45 +0800

    04 - 09 - 07 对接阿里云视频点播

--- app/Models/ResourceVideo.php
+++ app/Models/ResourceVideo.php
@@ -2,6 +2,10 @@
 
 namespace App\Models;
 
+use AliCloud\Core\DefaultAcsClient;
+use AliCloud\Core\Profile\DefaultProfile;
+use AliCloud\Vod\GetVideoPlayAuthRequest;
+use Exception;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -10,4 +14,35 @@ class ResourceVideo extends Model
     //
     use SoftDeletes;
     protected $fillable = ['resource_id', 'ali_id'];
+
+    /**
+     * 获取aliyun 视频ID的playAuth
+     *
+     * @return void
+     */
+    public function getPlayAuthAttribute(){
+        
+        //使用
+        $access_key_id = setting('ali_access');
+        $access_key_secret = setting('ali_secret');
+        $region_id = setting('ali_region');  
+
+        try{
+            $profile = DefaultProfile::getProfile($region_id,$access_key_id,$access_key_secret);
+            $client = new DefaultAcsClient($profile);
+              
+            $videoId = $this->ali_id;
+            $req = new GetVideoPlayAuthRequest();
+            $req->setVideoId($videoId);
+            $req->setAcceptFormat("JSON");
+            $response = $client->getAcsResponse($req);
+        }catch (Exception $e){
+            exit($e);
+        }    
+        
+        return $response;
+  
+    }
+
+
 }
