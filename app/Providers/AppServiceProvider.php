f6f2965ca82aa5fad0e77efabc1f029fd7e4c5c2
bobi <983430207@qq.com>
2020-03-20 11:20:14 +0800

    解决mysql版本太低的问题

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         //页头组件
         Blade::component('admin.components.page_title', 'page_title');
     }
